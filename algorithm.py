f1c9593109e5b9209839a476f12281e725721756
Yaodong Zhao <zhao@yaodong.org>
2017-07-19 22:31:43 -0600

    Finish drawing graphs in high dimension

--- algorithm.py
+++ algorithm.py
@@ -8,14 +8,27 @@ from scipy.sparse.csgraph import shortest_path
 class GraphDrawing:
 
     def __init__(self, graph_file, dimension=50):
-        self.graph_file = graph_file
         self.dimension = dimension
 
+        self.graph = self.load_graph(graph_file)
+        self.max_node = self.find_max_node(self.graph)
+        self.distance_matrix = self.distance_matrix(self.graph, self.max_node)
+
     def draw(self):
-        graph = self.load_graph(self.graph_file)
-        pivots = self.choose_pivot_points(graph, self.dimension)
+        pivot_nodes = self.choose_pivot_points(self.graph, self.dimension)
+        coordinates = map(
+            lambda i: tuple(self.compute_coordinates(i, pivot_nodes)),
+            pivot_nodes
+        )
+
         #self.draw_in_high_dimension(pivots, graph)
         #self.project_into_low_dimension()
+
+    @staticmethod
+    def find_max_node(graph):
+        return max([
+            *list(max(graph, key=lambda i: i[0]))[0:2],
+            *list(max(graph, key=lambda i: i[1]))[0:2]])
 
     @staticmethod
     def load_graph(graph_file):
@@ -30,26 +43,26 @@ class GraphDrawing:
         pivots = []
         coordinates = []
 
-        max_node_id = max([
+        max_node = max([
             *list(max(graph, key=lambda i: i[0]))[0:2],
             *list(max(graph, key=lambda i: i[1]))[0:2]])
 
-        distance_matrix = cls.distance_matrix(graph, max_node_id)
+        distance_matrix = cls.distance_matrix(graph, max_node)
 
         # choose the first one randomly from the graph
-        first = randint(0, max_node_id) + 1
+        first = randint(0, max_node) + 1
         pivots = [first]
 
         # find next by k-center problem
         for _ in range(0, dimension):
-            next_pivot = cls.k_center(distance_matrix, pivots, max_node_id)
+            next_pivot = cls.k_center(distance_matrix, pivots, max_node)
             pivots.append(next_pivot)
 
-        return None
+        return pivots
 
     @classmethod
-    def distance_matrix(cls, graph, max_node_id):
-        graph_matrix = lil_matrix((max_node_id, max_node_id))
+    def distance_matrix(cls, graph, max_node):
+        graph_matrix = lil_matrix((max_node, max_node))
 
         for node_from, node_to, weight in graph:
             graph_matrix[node_from - 1, node_to - 1] = weight
@@ -60,12 +73,16 @@ class GraphDrawing:
                              unweighted=False)
 
     @classmethod
-    def k_center(cls, distance_matrix, pivots, max_node_id):
+    def k_center(cls, distance_matrix, pivots, max_node):
         distances = []
-        for i in range(1, max_node_id + 1):
+        for i in range(1, max_node + 1):
             if i not in pivots:
-                nearest_pivot = min(pivots, key=lambda p: distance_matrix[i-1,p-1])
-                nearest_dis = distance_matrix[i-1, nearest_pivot-1]
+                matrix_i = i - 1
+                nearest_pivot = min(pivots, key=lambda p: distance_matrix[matrix_i, p - 1])
+                nearest_dis = distance_matrix[matrix_i, nearest_pivot - 1]
                 distances.append((i, nearest_dis))
 
         return max(distances, key=lambda d: d[1])[0]
+
+    def compute_coordinates(self, node, pivot_nodes):
+        return (self.distance_matrix[node - 1, p - 1] for p in pivot_nodes)
