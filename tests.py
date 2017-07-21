6ee10612b589989550a79c101ee6264ff3c2f88f
Yaodong Zhao <zhao@yaodong.org>
2017-07-21 17:10:55 -0600

    Update test cases

--- tests.py
+++ tests.py
@@ -9,9 +9,10 @@ class TestGraphDrawing(unittest.TestCase):
     The test data is a circle of 99 nodes. Any edge's weight is 1.
     """
 
-    def setUp(self):
-        self.drawing = GraphDrawing('data/tests.csv', dimension=8, first=1)
-        self.drawing.draw()
+    @classmethod
+    def setUpClass(cls):
+        cls.drawer = GraphDrawing(dimension=8)
+        cls.drawer.transform('data/tests.csv', first_node=1)
 
     def test_graph(self):
         """
@@ -19,20 +20,20 @@ class TestGraphDrawing(unittest.TestCase):
         """
         test_row = [randint(1, 98) for _ in range(0, 10)]
         for row in test_row:
-            self.assertEqual(tuple(self.drawing.graph[row - 1]), (row, row + 1, 1.0))
+            self.assertEqual(tuple(self.drawer.graph[row - 1]), (row, row + 1, 1.0))
 
     def test_max_node(self):
         """
         The max node should be 99
         """
-        self.assertEqual(self.drawing.max_node, 99)
+        self.assertEqual(self.drawer.node_count, 99)
 
     def test_distance_matrix(self):
         """
         The distance from i-th node to (i+1)-th node should be 1.
         The distance from the last node (99) to the first node (1) should be 1.
         """
-        distance_matrix = self.drawing.distance_matrix
+        distance_matrix = self.drawer.distance_matrix
         self.assertEqual(distance_matrix[1, 2], 1)
         self.assertEqual(distance_matrix[1, 3], 2)
         self.assertEqual(distance_matrix[98, 0], 1)
@@ -43,16 +44,16 @@ class TestGraphDrawing(unittest.TestCase):
         Select 8 pivot nodes from a 99 node circle.
         These pivot nodes should evenly distributed on the circle.
         """
-        self.assertEqual(self.drawing.pivot_nodes,
+        self.assertEqual(self.drawer.pivot_nodes,
                          [1, 50, 75, 25, 13, 37, 62, 87])
 
-    def test_coordinates(self):
+    def test_high_dimensional_points(self):
         """
         The i-th pivot's coordinate should has a 0.0 on i-th axie.
         """
-        for index, pivot in enumerate(self.drawing.pivot_nodes):
+        for index, pivot in enumerate(self.drawer.pivot_nodes):
             self.assertEqual(
-                self.drawing.coordinates[pivot - 1][index],
+                self.drawer.points[pivot - 1][index],
                 0.0)
 
 
